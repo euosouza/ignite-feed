eaa082046c2a8d369017e6036f7c4b116bf1fac4
victor-souza <euosouza@gmail.com>
2023-08-04 18:10:04 -0300

    adicionando arquivo global.scss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 import './assets/styles/reset.scss';
+import './assets/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
