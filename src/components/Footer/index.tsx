23ab852e578ce96a124f255e1e59d64499b9b304
victor-souza <euosouza@gmail.com>
2023-08-04 19:17:17 -0300

    criando componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import './footer.scss';
+
+export function Footer() {
+  return(
+    <footer className="footer">
+      <p>
+        Desenvolvido por <a href="https://www.linkedin.com/in/victor-souza-costa/">Victor Souza</a> ❤ -
+        Projeto de <a href="https://www.rocketseat.com.br/">@Rocketseat</a> 🚀
+      </p>
+    </footer>
+  )
+}
