7f7860e67a33fb079a7aeb746ac60f246f85ccfd
victor-souza <euosouza@gmail.com>
2023-08-04 21:22:24 -0300

    adicionando e ajustando marcacao para componentes no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import '@assets/styles/app.scss';
+
 import { Footer } from "@components/Footer"
 import { Header } from "@components/Header"
 
@@ -5,6 +7,14 @@ function App() {
   return (
     <>
       <Header />
+      <main className="wrapper-main">
+        <aside>
+          <span>Sidebar</span>
+        </aside>
+        <div className="list-posts">
+          <span>Lista de posts</span>
+        </div>
+      </main>
       <Footer />
     </>
   )
