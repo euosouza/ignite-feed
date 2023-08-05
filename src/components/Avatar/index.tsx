180190acd808862f3fe8d6d13f6f6ff059cb997b
victor-souza <euosouza@gmail.com>
2023-08-05 00:26:07 -0300

    atualizando tipagem dos componentes Post e Avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,16 +1,16 @@
-import './avatar.scss';
-import '../../assets/styles/common/utils.scss';
+import { ImgHTMLAttributes } from 'react';
 
-interface AvatarProps {
+import './avatar.scss';
+
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   hasBorder?: boolean;
-  src: string;
 }
 
-export function Avatar({ hasBorder = true, src }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={ hasBorder ? "avatar-with-border" : "avatar" }
-      src={src} alt="Avatar do usuário"
+      { ...props }
     />
   );
 }
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -14,7 +14,7 @@ interface Author {
 }
 
 interface Content {
-  type: string;
+  type: "paragraph" | "link";
   content: string;
 }
 
